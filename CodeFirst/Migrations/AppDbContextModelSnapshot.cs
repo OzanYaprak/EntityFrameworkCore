f49a903e8a8be85a2b8ee70516b17977104d7ee5
OZAN ERHAN YAPRAK <ozanerhanyaprak@icloud.com>
2024-08-05 23:35:09 +0300

    #

--- CodeFirst/Migrations/AppDbContextModelSnapshot.cs
+++ CodeFirst/Migrations/AppDbContextModelSnapshot.cs
@@ -43,7 +43,7 @@ namespace CodeFirst.Migrations
 
                     b.HasKey("Id");
 
-                    b.ToTable("Products");
+                    b.ToTable("Products", (string)null);
                 });
 #pragma warning restore 612, 618
         }
--- CodeFirst/Models/Product.cs
+++ CodeFirst/Models/Product.cs
@@ -11,7 +11,7 @@ namespace CodeFirst.Models
 	{
         public int Id { get; set; }
         public string Name { get; set; }
-        public decimal Price { get; set; }
+        public int Price { get; set; }
         public int Stock { get; set; }
         public int Barcode { get; set; }
     }
